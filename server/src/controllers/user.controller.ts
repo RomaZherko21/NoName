a125886997e779340218fdcef6bb087b3ca7de4a
Roman Zherko <zherko.r@parimatch.local>
2022-05-13 14:04:48 +0300

    Server: delete old avatar after upload

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express'
 import createError from 'http-errors'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import fs from 'fs'
+import path from 'path'
 
 import UserModel from '../models/user.model'
 
@@ -109,10 +111,24 @@ class UserController {
       const { id } = req.body
 
       if (file && id) {
-        const [result] = await UserModel.update(
-          { avatar: file.filename },
-          { where: { id } }
-        )
+        const data: any = await UserModel.findByPk(id)
+
+        if (data.avatar) {
+          const filePath = path.join(
+            path.dirname(require.main.path),
+            '/uploads',
+            '/avatar',
+            data.avatar
+          )
+          if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) => {
+              if (err) throw err
+            })
+          }
+        }
+
+        await UserModel.update({ avatar: file.filename }, { where: { id } })
+
         res.status(200).json({ url: file.filename })
       } else {
         next(createError(400, 'Avatar wasnt uploaded'))
