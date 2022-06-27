38c8bc96951b089cc5ad0d1796785d1c687632ab
Roman Zherko <zherko.r@parimatch.local>
2022-06-27 13:30:32 +0300

    Common: edit husky config

--- server/src/controllers/item/queries.ts
+++ server/src/controllers/item/queries.ts
@@ -1,6 +1,6 @@
 export const getItemListQuery = (
   orderBy = { field: 'createdAt', type: 'ASC' }
 ) => `SELECT items.*, users.avatar  FROM items JOIN users 
-ON items.userId = users.id 
-ORDER BY items.${orderBy.field} ${orderBy.type} 
-LIMIT :limit OFFSET :offset;`
+        ON items.userId = users.id 
+        ORDER BY items.${orderBy.field} ${orderBy.type} 
+        LIMIT :limit OFFSET :offset;`
