787862c317507b198314492bdba0fb131ba9f326
Roman Zherko <zherko.r@parimatch.local>
2022-07-15 19:48:28 +0300

    MySQL: edit setup config

--- MySQL/setup.sql
+++ MySQL/setup.sql
@@ -1,22 +1,13 @@
-CREATE TABLE IF NOT EXISTS `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50),
-  `description` varchar(50),
-  `createdAt` varchar(50),
-  `image` varchar(50),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50),
-  `surname` varchar(50),
-  `email` varchar(50) NOT NULL UNIQUE,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255),
+  `surname` varchar(255),
+  `email` varchar(255) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
-  `role` varchar(50),
-  `avatar` varchar(50),
+  `role` varchar(255),
+  `avatar` varchar(255),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `users` (email, password) 
 VALUES('admin@gmail.com', '$2b$10$qrN6JkJjKG8fa2tEUvyhb.2Hfgpx8w4l7/Mb3y4/rZXdMMRGWHEkS');
