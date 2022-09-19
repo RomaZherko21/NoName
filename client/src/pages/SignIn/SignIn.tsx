29a1bd82c14e19813b7333ae2ec46fcb4fab2091
Roman Zherko <zherko.r@parimatch.local>
2022-09-19 17:49:16 +0300

    Client: refactor signIn page

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -2,13 +2,11 @@ import * as yup from 'yup'
 import { useFormik } from 'formik'
 import { observer } from 'mobx-react-lite'
 import { useTranslation } from 'react-i18next'
-import { Button, Grid, TextField } from '@mui/material'
+import { Avatar, Button, Container, Paper, Stack, TextField, Typography } from '@mui/material'
 
 import { emailValidation, passwordValidation } from 'shared/validations'
 import { useRootStore } from 'stores'
 import { TUser } from 'shared/types'
-
-import styles from './Styles.module.scss'
 
 const validationSchema = yup.object().shape({
   email: emailValidation(),
@@ -31,40 +29,49 @@ function SignIn() {
   })
 
   return (
-    <form onSubmit={formik.handleSubmit} className={styles.centered}>
-      <Grid item container spacing={2} direction="column" xs={11} md={6}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            fullWidth
-            id="email"
-            name="email"
-            label="Email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            fullWidth
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Button color="primary" variant="contained" fullWidth type="submit">
-            {t('common.confirm')}
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
+    <Container maxWidth="xs" sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
+      <Paper
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>Logo</Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
+          <Stack justifyContent="center" spacing={2} sx={{ padding: '20px' }}>
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button color="primary" variant="contained" type="submit">
+              {t('common.confirm')}
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
