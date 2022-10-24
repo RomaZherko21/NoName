daec45f4b4ca5c02c47373574132b3cb52d083a0
Roman <zherko.r@parimatch.local>
2022-10-24 14:30:20 +0300

    refactor(client): add 404 page translation

--- client/src/pages/404/404.tsx
+++ client/src/pages/404/404.tsx
@@ -1,64 +1,68 @@
 import { Helmet } from 'react-helmet'
+import { useTranslation } from 'react-i18next'
 import { Box, Button, Container, Typography } from '@mui/material'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 import notFoundImg from 'assets/images/404.svg'
 
-const NotFound = () => (
-  <>
-    <Helmet>
-      <title>404 | Page not found</title>
-      <meta name="description" content="404 | Page not found" />
-    </Helmet>
-    <Box
-      component="main"
-      sx={{
-        alignItems: 'center',
-        display: 'flex',
-        flexGrow: 1,
-        minHeight: '100%',
-      }}
-    >
-      <Container maxWidth="md">
-        <Box
-          sx={{
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <Typography align="center" color="textPrimary" variant="h1">
-            404: The page you are looking for isn’t here
-          </Typography>
-          <Typography align="center" color="textPrimary" variant="subtitle2">
-            You either tried some shady route or you came here by mistake. Whichever it is, try
-            using the navigation
-          </Typography>
-          <Box sx={{ textAlign: 'center' }}>
-            <img
-              alt="Under development"
-              src={notFoundImg}
-              style={{
-                marginTop: 50,
-                display: 'inline-block',
-                maxWidth: '100%',
-                width: 560,
-              }}
-            />
-          </Box>
-          <Button
-            component="a"
-            startIcon={<ArrowBackIcon fontSize="small" />}
-            sx={{ mt: 3 }}
-            variant="contained"
-            href="/"
+const NotFound = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Helmet>
+        <title>404 | Page not found</title>
+        <meta name="description" content="404 | Page not found" />
+      </Helmet>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%',
+        }}
+      >
+        <Container maxWidth="md">
+          <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
           >
-            Go back
-          </Button>
-        </Box>
-      </Container>
-    </Box>
-  </>
-)
+            <Typography align="center" color="textPrimary" variant="h1">
+              {t('sentence:notFound.title')}
+            </Typography>
+            <Typography align="center" color="textPrimary" variant="subtitle2">
+              {t('sentence:notFound.subtitle')}
+            </Typography>
+            <Box sx={{ textAlign: 'center' }}>
+              <img
+                alt="Under development"
+                src={notFoundImg}
+                style={{
+                  marginTop: 50,
+                  display: 'inline-block',
+                  maxWidth: '100%',
+                  width: 560,
+                }}
+              />
+            </Box>
+            <Button
+              component="a"
+              startIcon={<ArrowBackIcon fontSize="small" />}
+              sx={{ mt: 3 }}
+              variant="contained"
+              href="/"
+            >
+              {t('actions.goBack')}
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  )
+}
 
 export default NotFound
