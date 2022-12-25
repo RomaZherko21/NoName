3c2d3dc13bc79c87db696263b3ce12922ab04a62
Roman <zherko.r@parimatch.local>
2022-12-25 21:36:39 +0300

    feat(root-0): change commit message validation

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,31 +2,19 @@ module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'scope-case': () => [2, 'always', 'kebab-case'],
-    'scope-validation': () => [2, 'always', ['client', 'node-api', 'mysql', 'go-api', 'root']],
+    'scope-regexp-validation': () => [2, 'always', /^(FE|BE|root)-[0-9]+$/],
   },
   plugins: [
     {
       rules: {
-        'scope-validation': (parsed, when = 'always', scope = []) => {
+        'scope-regexp-validation': (parsed, when = 'always', regExp) => {
           const isInvertedRule = when === 'never'
 
-          let result = true
-
-          if (parsed.scope) {
-            parsed.scope.split(', ').forEach((word) => {
-              if (!scope.includes(word)) {
-                result = false
-              }
-            })
-          } else {
-            result = false
-          }
+          const result = regExp.test(parsed.scope)
 
           return [
             isInvertedRule ? !result : result,
-            `Scope must be in [${scope.join(', ')}], f.e. (${scope[0]}, ${scope[1]}) or (${
-              scope[0]
-            })`,
+            `Scope must be match regex ${regExp}, f.e. FE-33`,
           ]
         },
       },
