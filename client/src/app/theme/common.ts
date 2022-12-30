995d7b82a8c5d1c408cc2e6c6c36a5b9ef6df54e
Roman <zherko.r@parimatch.local>
2022-12-31 00:08:37 +0300

    feat(FE-14): change global fonts

--- client/src/app/theme/common.ts
+++ client/src/app/theme/common.ts
@@ -1,3 +1,6 @@
+const PRIMARY_FONT = ['Noto Sans Mono', 'Arial', 'sans-serif'].join(',')
+const SECONDARY_FONT = ['Manrope', 'Arial', 'sans-serif'].join(',')
+
 const commonTheme = {
   zIndex: {
     appBar: 1250,
@@ -5,41 +8,66 @@ const commonTheme = {
   typography: {
     htmlFontSize: 18,
     fontSize: 16,
+    fontFamily: PRIMARY_FONT,
+
+    subtitle1: {
+      fontFamily: SECONDARY_FONT,
+    },
+    subtitle2: {
+      fontFamily: SECONDARY_FONT,
+    },
+    body1: {
+      fontFamily: PRIMARY_FONT,
+    },
+    body2: {
+      fontFamily: PRIMARY_FONT,
+    },
+    caption: {
+      fontFamily: SECONDARY_FONT,
+    },
+    overline: {
+      fontFamily: SECONDARY_FONT,
+    },
     button: {
       fontWeight: 400,
       textTransform: 'none',
+      fontFamily: PRIMARY_FONT,
     },
-    fontFamily:
-      '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
     h1: {
       fontWeight: 700,
       fontSize: '3.5rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
     h2: {
       fontWeight: 700,
       fontSize: '3rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
     h3: {
       fontWeight: 700,
       fontSize: '2.25rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
     h4: {
       fontWeight: 700,
       fontSize: '2rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
     h5: {
       fontWeight: 600,
       fontSize: '1.5rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
     h6: {
       fontWeight: 600,
       fontSize: '1.125rem',
       lineHeight: 1.375,
+      fontFamily: PRIMARY_FONT,
     },
   },
   shape: {
--- client/src/app/theme/theme.tsx
+++ client/src/app/theme/theme.tsx
@@ -1,5 +1,5 @@
 import { createContext, FC, useEffect, useMemo } from 'react'
-import { createTheme, ThemeProvider } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 import { useBoolState } from 'shared/hooks'
 
