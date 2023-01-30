5fa6cc9150e6ae0b07f3a277493a6c467787c980
Roman <zherko.r@parimatch.local>
2023-01-31 00:08:51 +0300

    refactor(FE-0): edit popup

---
+++ client/src/shared/ui/Popover/PopupMenu.tsx
@@ -0,0 +1,66 @@
+import { useNavigate } from 'react-router-dom'
+import { observer } from 'mobx-react-lite'
+import { useTranslation } from 'react-i18next'
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material'
+
+import Popover from './Popover'
+
+interface Props {
+  config: Array<{
+    Icon: JSX.Element
+    text: string
+    linkTo?: string
+    onClick?: (args?: any) => void
+    key?: string
+  }>
+  ActionButton: (args: any) => JSX.Element
+  id?: number
+  selectCondition?: string
+}
+
+const PopupMenu = ({ config, ActionButton, id, selectCondition }: Props) => {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <Popover activateElement={(_, handleOpen) => ActionButton({ onClick: handleOpen })}>
+        <List sx={{ minWidth: 200, p: 0 }}>
+          {config.map((item) => (
+            <ListItem disablePadding key={item.text}>
+              <ListItemButton
+                onClick={() => {
+                  if (item.linkTo) {
+                    navigate(item.linkTo)
+                  } else {
+                    item.onClick?.(id)
+                  }
+                }}
+                sx={{ px: 1, py: 0.5 }}
+              >
+                <ListItemIcon>{item.Icon}</ListItemIcon>
+                <ListItemText>
+                  <Typography
+                    variant="body2"
+                    color={selectCondition === item.key ? 'text.primary' : 'text.secondary'}
+                  >
+                    {t(item.text)}
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Popover>
+    </>
+  )
+}
+
+export default observer(PopupMenu)
--- client/src/shared/ui/Popover/index.ts
+++ client/src/shared/ui/Popover/index.ts
@@ -1,1 +1,2 @@
+export { default as PopupMenu } from './PopupMenu'
 export { default as Popover } from './Popover'
