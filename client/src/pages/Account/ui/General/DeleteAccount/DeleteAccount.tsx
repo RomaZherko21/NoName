9324a887c8d02257f3d80926090bb1ada36921dd
Ira Siahliuk <70015292+IrinaSiahliuk@users.noreply.github.com>
2023-02-07 00:46:06 +0300

    Fe 46 add edit user page (#48)
    * feat(FE-46): change edit user page
    
    * fix(FE-46): fix
    
    ---------
    
    Co-authored-by: Roma Zherko <49365164+RomaZherko21@users.noreply.github.com>
    Co-authored-by: Roman <zherko.r@parimatch.local>

--- client/src/pages/Account/ui/General/DeleteAccount/DeleteAccount.tsx
+++ client/src/pages/Account/ui/General/DeleteAccount/DeleteAccount.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import { useTranslation } from 'react-i18next'
-import { Box, Button, Card, CardContent, Grid, Typography } from '@mui/material'
+import { Box, Button, Grid, Paper, Typography } from '@mui/material'
 
 import { useDialog } from 'shared/hooks'
 
@@ -16,37 +16,33 @@ const DeleteAccount = () => {
   )
 
   return (
-    <Card>
-      <CardContent>
-        <Grid container spacing={3}>
-          <Grid item lg={4} md={6} xs={12}>
-            <Typography color="textPrimary" variant="h5">
-              {t('user:actions.deleteAccount')}
-            </Typography>
-          </Grid>
-          <Grid item lg={8} md={6} xs={12}>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <Typography color="textPrimary" variant="body1">
-                {t('sentences:deleteAccount')}
-              </Typography>
-              <Button
-                onClick={showConfirmationModal}
-                sx={{ mt: 3, width: 'fit-content' }}
-                color="error"
-                variant="outlined"
-              >
-                {t('user:actions.deleteAccount')}
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+    <Grid component={Paper} sx={{ p: 4, width: '100%', mt: 2 }} container>
+      <Grid item lg={4} md={6} xs={12}>
+        <Typography color="textPrimary" variant="h6">
+          {t('user:actions.deleteAccount')}
+        </Typography>
+      </Grid>
+      <Grid item lg={8} md={6} xs={12}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Typography color="textPrimary" variant="body1">
+            {t('sentences:deleteAccount')}
+          </Typography>
+          <Button
+            onClick={showConfirmationModal}
+            sx={{ mt: 3, width: 'fit-content' }}
+            color="error"
+            variant="outlined"
+          >
+            {t('user:actions.deleteAccount')}
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
 
--- client/src/pages/Account/ui/General/ProfileForm/ProfileForm.tsx
+++ client/src/pages/Account/ui/General/ProfileForm/ProfileForm.tsx
@@ -4,11 +4,12 @@ import { observer } from 'mobx-react-lite'
 import { useTranslation } from 'react-i18next'
 import { Formik } from 'formik'
 import { toast } from 'react-toastify'
-import { Button, Divider, Grid, Stack } from '@mui/material'
+import { Button, Grid, Paper, Stack, Typography, Box } from '@mui/material'
 
 import { useRootStore } from 'stores'
 import { InputField, SelectField, Spinner, UploadImage } from 'shared/ui'
 import { commonStringValidation, emailValidation } from 'shared/validations'
+import { getFullName } from 'shared/helpers'
 import { GENDER, ROLES } from 'shared/consts'
 
 const ProfileForm = () => {
@@ -33,24 +34,12 @@ const ProfileForm = () => {
 
   return (
     <>
-      <Stack sx={{ mb: 2, width: 'fit-content' }}>
-        {loading.has ? (
-          <Spinner />
-        ) : (
-          <UploadImage
-            width={64}
-            height={64}
-            handleUploadClick={handleUploadClick}
-            imageUrl={user.getPhotoUrl()}
-          />
-        )}
-      </Stack>
-      <Divider />
       <Formik
         initialValues={{
           name: user.name,
           surname: user.surname,
           middle_name: user.middle_name,
+          full_name: getFullName(user.name, user.surname, user.middle_name),
           email: user.email,
           tel_number: user.tel_number,
           role: user.role,
@@ -65,35 +54,96 @@ const ProfileForm = () => {
       >
         {({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
-            <Grid container spacing={3}>
-              <Grid item md={4} xs={12}>
-                <InputField field="name" label="user:name" />
+            <Grid component={Paper} sx={{ p: 4, width: '100%', mb: 2 }} container>
+              <Grid xs={12} md={4}>
+                <Typography variant="h6">{t('user:basicDetails')}</Typography>
               </Grid>
-              <Grid item md={4} xs={12}>
-                <InputField field="surname" label="user:surname" />
+
+              <Grid item xs={12} md={8}>
+                <Stack sx={{ mb: 3, width: 'fit-content' }}>
+                  {loading.has ? (
+                    <Spinner />
+                  ) : (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Box
+                        sx={{ borderStyle: 'dashed', borderWidth: 1, borderRadius: '50%', p: 0.5 }}
+                      >
+                        <UploadImage
+                          width={80}
+                          height={80}
+                          handleUploadClick={handleUploadClick}
+                          imageUrl={user.getPhotoUrl()}
+                        />
+                      </Box>
+                      <Button
+                        variant="text"
+                        size="small"
+                        sx={{ color: (theme) => theme.palette.primary.contrastText }}
+                      >
+                        {t('actions.change')}
+                      </Button>
+                    </Box>
+                  )}
+                </Stack>
+
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <InputField field="full_name" label="user:fullName" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InputField field="tel_number" label="user:telephoneNumber" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InputField field="email" label="user:email" />
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item md={4} xs={12}>
-                <InputField field="middle_name" label="user:middleName" />
+            </Grid>
+
+            <Grid component={Paper} sx={{ p: 4, width: '100%', mb: 2 }} container>
+              <Grid xs={12} md={4}>
+                <Typography variant="h6">{t('user:meta')}</Typography>
               </Grid>
-              <Grid item md={8} xs={12}>
-                <InputField field="tel_number" label="user:telephoneNumber" />
+
+              <Grid container spacing={3} xs={12} md={8}>
+                <Grid item md={6} xs={12}>
+                  <SelectField field="gender" label="user:gender" options={GENDER} />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <SelectField field="role" label="user:role" options={ROLES} />
+                </Grid>
+                <Grid item md={12} xs={12}>
+                  <InputField field="date_of_birth" label="user:dateOfBirth" />
+                </Grid>
               </Grid>
-              <Grid item md={4} xs={12}>
-                <SelectField field="gender" label="user:gender" options={GENDER} />
-              </Grid>
-              <Grid item md={8} xs={12}>
-                <InputField field="email" label="user:email" />
-              </Grid>
-              <Grid item md={4} xs={12}>
-                <SelectField field="role" label="user:role" options={ROLES} />
-              </Grid>
-              <Grid item md={12} xs={12}>
-                <InputField field="date_of_birth" label="user:dateOfBirth" />
-              </Grid>
-              <Grid item xs={12}>
-                <Button color="primary" variant="contained" type="submit">
+            </Grid>
+
+            <Grid component={Paper} sx={{ p: 4, width: '100%', mb: 1 }} container>
+              <Grid item lg={4} md={6} xs={12}>
+                <Typography color="textPrimary" variant="h6">
                   {t('common.confirm')}
-                </Button>
+                </Typography>
+              </Grid>
+
+              <Grid item lg={8} md={6} xs={12}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Typography color="textPrimary" variant="body1">
+                    {t('sentences:confirm')}
+                  </Typography>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    sx={{ mt: 3, width: 'fit-content' }}
+                  >
+                    {t('common.confirm')}
+                  </Button>
+                </Box>
               </Grid>
             </Grid>
           </form>
