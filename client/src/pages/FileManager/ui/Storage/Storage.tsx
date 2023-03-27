50cc3f9639311fe2b5bc0df37e9c67fdff149426
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2023-03-27 16:50:59 +0300

    feat(FE-74): added file extensions to file page

--- client/src/pages/FileManager/ui/Storage/Storage.tsx
+++ client/src/pages/FileManager/ui/Storage/Storage.tsx
@@ -13,7 +13,10 @@ import {
 import { HiOutlineLightningBolt } from 'react-icons/hi'
 
 import { CircleDevider } from 'shared/ui'
+import mp4 from 'shared/assets/images/fileFormat/mp4.svg'
+import png from 'shared/assets/images/fileFormat/png.svg'
 import pdf from 'shared/assets/images/fileFormat/pdf.svg'
+import other from 'shared/assets/images/fileFormat/other.svg'
 
 const Storage = () => {
   const { t } = useTranslation()
@@ -43,6 +46,32 @@ const Storage = () => {
           <ListItem sx={{ display: 'flex', alignItems: 'center', py: 1, px: 0 }}>
             <ListItemIcon>
               <Box sx={{ mr: 2 }}>
+                <img alt="Mp4" src={mp4} />
+              </Box>
+            </ListItemIcon>
+            <ListItemText sx={{ my: 0.75 }}>
+              <Typography variant="caption">MP4</Typography>
+              <Typography variant="body2" color="text.secondary">
+                22.75 GB <CircleDevider sx={{ backgroundColor: 'text.secondary' }} /> 25 items
+              </Typography>
+            </ListItemText>
+          </ListItem>
+          <ListItem sx={{ display: 'flex', alignItems: 'center', py: 1, px: 0 }}>
+            <ListItemIcon>
+              <Box sx={{ mr: 2 }}>
+                <img alt="Png" src={png} />
+              </Box>
+            </ListItemIcon>
+            <ListItemText sx={{ my: 0.75 }}>
+              <Typography variant="caption">PNG</Typography>
+              <Typography variant="body2" color="text.secondary">
+                54.69 GB <CircleDevider sx={{ backgroundColor: 'text.secondary' }} /> 591 items
+              </Typography>
+            </ListItemText>
+          </ListItem>
+          <ListItem sx={{ display: 'flex', alignItems: 'center', py: 1, px: 0 }}>
+            <ListItemIcon>
+              <Box sx={{ mr: 2 }}>
                 <img alt="Pdf" src={pdf} />
               </Box>
             </ListItemIcon>
@@ -50,6 +79,19 @@ const Storage = () => {
               <Typography variant="caption">PDF</Typography>
               <Typography variant="body2" color="text.secondary">
                 412.39 MB <CircleDevider sx={{ backgroundColor: 'text.secondary' }} /> 95 items
+              </Typography>
+            </ListItemText>
+          </ListItem>
+          <ListItem sx={{ display: 'flex', alignItems: 'center', py: 1, px: 0 }}>
+            <ListItemIcon>
+              <Box sx={{ mr: 2 }}>
+                <img alt="Other" src={other} />
+              </Box>
+            </ListItemIcon>
+            <ListItemText sx={{ my: 0.75 }}>
+              <Typography variant="caption">Other</Typography>
+              <Typography variant="body2" color="text.secondary">
+                261.43 MB <CircleDevider sx={{ backgroundColor: 'text.secondary' }} /> 210 items
               </Typography>
             </ListItemText>
           </ListItem>
