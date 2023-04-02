47a2d1cb0711d9b7a02acce1189fbbf4a0479753
Roman <zherko.r@parimatch.local>
2023-04-02 20:36:46 +0300

    feat(FE-75): add icons

--- client/src/pages/Kanban/ui/KanbanPopupConfig.tsx
+++ client/src/pages/Kanban/ui/KanbanPopupConfig.tsx
@@ -1,14 +1,19 @@
+import { FaTrashAlt } from 'react-icons/fa'
+import { BsBrushFill } from 'react-icons/bs'
+
 export const getKanbanPopupConfig = (id: number) => [
-    {
-      text: 'actions.clear',
-      onClick: () => {
-        console.log(id)
-      }
-    },
-    {
-      text: 'actions.delete',
-      onClick: () => {
-        console.log(id)
-      }
+  {
+    Icon: <BsBrushFill />,
+    text: 'actions.clear',
+    onClick: () => {
+      console.log(id)
     }
-  ]
+  },
+  {
+    Icon: <FaTrashAlt />,
+    text: 'actions.delete',
+    onClick: () => {
+      console.log(id)
+    }
+  }
+]
