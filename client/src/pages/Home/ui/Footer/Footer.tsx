76b8b4eafaf83eb39b32d745453db404377c30fb
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2023-04-17 02:50:50 +0300

    fix(FE-79): improved code, added FooterLink component

--- client/src/pages/Home/ui/Footer/Footer.tsx
+++ client/src/pages/Home/ui/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import { Grid, Typography, Divider, Link as MuiLink, Box } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
 import { ROUTES } from 'shared/consts'
+import { FooterLink } from './ui'
 
 import logo from 'shared/assets/images/logo/white-transparent-logo.svg'
 
@@ -31,172 +32,58 @@ function Footer() {
           <Typography variant="body2" color="text.secondary">
             {t('home:menu')}
           </Typography>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            Browse Components
-          </MuiLink>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            Documentation
-          </MuiLink>
+          {[
+            {
+              title: 'Browse Components',
+              path: ROUTES.HOME
+            },
+            {
+              title: 'Documentation',
+              path: ROUTES.HOME
+            }
+          ].map((category) => (
+            <FooterLink category={category} key={category.title} />
+          ))}
         </Grid>
 
         <Grid item xs={3} sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
           <Typography variant="body2" color="text.secondary">
             {t('home:legal')}
           </Typography>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            Terms & Conditions
-          </MuiLink>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            License
-          </MuiLink>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.CONTACT}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            Contact
-          </MuiLink>
+          {[
+            {
+              title: 'Terms & Conditions',
+              path: ROUTES.HOME
+            },
+            {
+              title: 'License',
+              path: ROUTES.HOME
+            },
+            {
+              title: 'Contact',
+              path: ROUTES.CONTACT
+            }
+          ].map((category) => (
+            <FooterLink category={category} key={category.title} />
+          ))}
         </Grid>
 
         <Grid item xs={3} sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
           <Typography variant="body2" color="text.secondary">
             {t('home:social')}
           </Typography>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            Instagram
-          </MuiLink>
-          <MuiLink
-            component={Link}
-            underline="hover"
-            variant="body2"
-            color="text.primary"
-            to={ROUTES.HOME}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: 1
-            }}
-          >
-            <Box
-              sx={{
-                mr: 1,
-                width: 12,
-                height: 2,
-                backgroundColor: 'primary.main'
-              }}
-            />
-            LinkedIn
-          </MuiLink>
+          {[
+            {
+              title: 'Instagram',
+              path: ROUTES.HOME
+            },
+            {
+              title: 'LinkedIn',
+              path: ROUTES.HOME
+            }
+          ].map((category) => (
+            <FooterLink category={category} key={category.title} />
+          ))}
         </Grid>
       </Grid>
 
