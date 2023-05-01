2d89d05545e6b41b983217a31ad91cd0cf84ffaa
Kiryl <bobkovum@gmail.com>
2023-05-01 12:07:07 +0300

    feat(FE-87): add and correct  kanban subtasks endpoint

--- client/src/services/api/kanbanSubtask.ts
+++ client/src/services/api/kanbanSubtask.ts
@@ -2,16 +2,16 @@ import { KanbanSubtask } from 'shared/types'
 
 import fetch from './fetch'
 
-const ENDPOINT_BASE = '/subtasks'
+const ENDPOINT_BASE = '/kanban'
 
 export const getKanbanSubtasks = async (id: number) =>
-  await fetch.get<KanbanSubtask>(`/kanban/tasks/${id}/${ENDPOINT_BASE}`)
+  await fetch.get<KanbanSubtask>(`${ENDPOINT_BASE}/tasks/${id}/subtasks`)
 
 export const createKanbanSubtask = async (subtask: KanbanSubtask, id: number) =>
-  await fetch.post(`/kanban/tasks/${id}/${ENDPOINT_BASE}`, subtask)
+  await fetch.post(`${ENDPOINT_BASE}/tasks/${id}/subtasks`, subtask)
 
 export const updateKanbanSubtask = async (id: number, subtask: KanbanSubtask) =>
-  await fetch.put(`/kanban/tasks/${id}/${ENDPOINT_BASE}/${id}`, subtask)
+  await fetch.put(`${ENDPOINT_BASE}/tasks/${id}/subtasks/${id}`, subtask)
 
 export const removeKanbanSubtask = async (id: number) =>
-  await fetch.delete(`/kanban/${ENDPOINT_BASE}/${id}`)
+  await fetch.delete(`${ENDPOINT_BASE}/subtasks/${id}`)
