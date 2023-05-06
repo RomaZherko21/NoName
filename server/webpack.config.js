d4c27c04d554a8041231769a557de62aba8f6893
Roma Zherko <49365164+RomaZherko21@users.noreply.github.com>
2023-05-06 12:14:07 +0300

    feat(FE-0): add prod bundle (#92)
    Co-authored-by: Roman <zherko.r@parimatch.local>

---
+++ server/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['node_modules', 'src'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
