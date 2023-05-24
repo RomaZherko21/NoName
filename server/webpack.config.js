274787dab3fc68bbde587524b005d80531432b2d
Roman <zherko.r@parimatch.local>
2023-05-24 09:22:26 +0300

    fix(BE-0): mr edits

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -4,12 +4,12 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.ts',
-  mode: 'production',
-  target: 'node',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
   },
+  mode: 'production',
+  target: 'node',
   resolve: {
     extensions: ['.ts', '.js'],
     plugins: [new TsconfigPathsPlugin()],
