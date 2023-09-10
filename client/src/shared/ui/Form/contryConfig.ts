61541c1018a01e996ada91fa73321b508a2971ce
roman.zherko <roman.zherko@pm.by>
2023-09-10 18:56:20 +0300

    feat(FE-112): mr

--- client/src/shared/ui/Form/contryConfig.ts
+++ client/src/shared/ui/Form/contryConfig.ts
@@ -5,8 +5,6 @@ interface CountryType {
   suggested?: boolean
 }
 
-// From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
-
 export const countries: readonly CountryType[] = [
   { code: 'AD', label: 'Andorra', phone: '376' },
   {
