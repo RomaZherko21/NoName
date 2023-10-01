c44a6cfe231c8d466a69c44b620aae994c49e67f
Kiryl Babkou <bobkovum@gmail.com>
2023-10-02 00:35:26 +0300

    formcoutryselect

--- client/src/entities/User/BasicInfo/BasicInfo.tsx
+++ client/src/entities/User/BasicInfo/BasicInfo.tsx
@@ -18,7 +18,7 @@ import { getListConfig } from './getListConfig'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import { getFullName, normalizePhone, getSplitName } from 'shared/helpers'
-import { FormPhoneNumber, InputField, Select } from 'shared/ui'
+import { FormPhoneNumber, InputField } from 'shared/ui'
 import {
   // fullNameValidation,
   emailValidation,
@@ -28,8 +28,6 @@ import {
 import { toast } from 'react-toastify'
 import FormCountrySelect from 'shared/ui/Form/FormContrySelect/FormCountrySelect'
 import { ProfileModel } from 'pages/Profile/model'
-import { FormSelect } from 'shared/ui/Form'
-import { FormPhoneNumber2 } from 'shared/ui/Form/FormPhoneNumber2'
 
 interface Props {
   user: BasicUserInfo & MetaUserInfo & UserLocation
@@ -123,32 +121,17 @@ function UserBasicDetails({ user }: Props) {
                   </ListItem>
                   <Divider />
                   <ListItem alignItems="center" sx={{ flexDirection: 'row', mt: '3px' }}>
-                    <FormCountrySelect />
+                    <FormCountrySelect label={t('user:nativeCountry')} field={'native_country'} />
                     {/* <InputField field={'native_country'} label={t('user:nativeCountry')} /> */}
                     <InputField field={'native_city'} label={t('user:nativeCity')} />
                   </ListItem>
                   <Divider />
                   <ListItem alignItems="center" sx={{ flexDirection: 'row', mt: '3px' }}>
-                    <FormPhoneNumber2
+                    <FormCountrySelect
                       field={'residence_country'}
                       label={t('user:residenceCountry')}
                     />
 
-                    {/* <Select
-                      value={'residence_country'}
-                      label={t('user:residenceCountry')}
-                      options={{}}
-                      onChange={function (e: any): void {
-                        throw new Error('Function not implemented.')
-                      }}
-                    /> */}
-
-                    {/* <FormSelect
-                      field={'residence_country'}
-                      label={t('user:residenceCountry')}
-                      options={{}}
-                    /> */}
-                    {/* <InputField field={'residence_country'} label={t('user:residenceCountry')} /> */}
                     <InputField field={'residence_city'} label={t('user:residenceCity')} />
                   </ListItem>
                   <Divider />
@@ -179,8 +162,6 @@ function UserBasicDetails({ user }: Props) {
                 {isEditActive ? t('actions.save') : t('actions.edit')}
               </Button>
             </CardActions>
-
-            <FormPhoneNumber2 field={'residence_country'} label={t('user:residenceCountry')} />
           </form>
         )}
       </Formik>
--- client/src/shared/ui/Form/FormContrySelect/FormCountrySelect.tsx
+++ client/src/shared/ui/Form/FormContrySelect/FormCountrySelect.tsx
@@ -1,13 +1,31 @@
-import { Autocomplete, Box, TextField } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { useFormikContext } from 'formik'
+import { Autocomplete, Box, InputAdornment, TextField } from '@mui/material'
+
 import { countries } from '../contryConfig'
 
-function FormCountrySelect() {
+interface Props {
+  label: string
+  field: string
+}
+
+function FormCountrySelect(props: Props) {
+  const { label, field } = props
+  const { t } = useTranslation()
+  const { setFieldValue, values } = useFormikContext<{
+    [key: string]: any
+  }>()
+
   return (
     <Autocomplete
       id="country-select-demo"
       sx={{ width: 400 }}
       options={countries}
       autoHighlight
+      fullWidth
+      onChange={(_, value) => {
+        setFieldValue(field, value)
+      }}
       getOptionLabel={(option) => option.label}
       renderOption={(props, option) => (
         <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
@@ -18,10 +36,22 @@ function FormCountrySelect() {
       renderInput={(params) => (
         <TextField
           {...params}
-          label="Choose a country"
-          inputProps={{
-            ...params.inputProps,
-            autoComplete: 'new-password' // disable autocomplete and autofill
+          label={t(label)}
+          variant="outlined"
+          value={values[field]}
+          InputProps={{
+            ...params.InputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+            startAdornment: (
+              <InputAdornment position="start">
+                <img
+                  loading="lazy"
+                  width="24"
+                  src={`/flags/${values[field]?.code?.toLowerCase()}.svg`}
+                  alt=""
+                />
+              </InputAdornment>
+            )
           }}
         />
       )}
